b5a455b1bf4fe667595986d8d91f3a85e944fd18
Alvaro <alvandroid.21@gmail.com>
2021-11-23 12:39:28 -0600

    Materias js

--- src/tools/materias.js
+++ src/tools/materias.js
@@ -1,6 +1,46 @@
 const ContadormMatarias = [
     {Muntimedios:0},
-    {PersonaYProyectoDeVida:0}
+    {Disenoydesarrollodevideojuegos:0},
+    {Comercioelectronico:0},
+    {Graficasporcomputadora:0},
+    {PersonaYProyectoDeVida:0},
+    {Propuestacristiana:0},
+    {Arteculturaysociedad:0},
+    {Responsabilidadsocialymundocontemporaneo:0},
+    {Apreciacioncinematografica:0},
+    {Tecnicasdeinvestigacion:0},
+    {Gestiondeproyectosinformaticos:0},
+    {Sistemasdeinformacionintegral:0},
+
+    {RedesIII:0},
+    {RedesIV:0},
+    {Serviciosdered:0},
+    {Seguridadyauditoriadesistemas:0},
+
+    {ProgramacionwebI:0},
+    {ProgramacionwebII:0},
+    {Programaciondesistemasmoviles:0},
+    {ProgramaciondesistemasmovilesII:0},
+    
+    {Electronica:0},
+    {Teoriadecircuitos:0},
+    {Sensoresyactuadores:0},
+    {SensoresyactuadoresII:0},
+    
+    {Introduccionalaprogramacion:0},
+    {Tecnologiasdeinformacionycomunicacion:0},
+    {ProgramacionI:0},
+    {ProgramacionII:0},
+    {Logicadigital:0},
+    {Estructuradedatos:0},
+    {Basesdedatos:0},
+    {RedesI:0},
+    {Lenguajeensamblador:0},
+    {Entornosdedesarrollointegrado:0},
+    {BasededatosII:0},
+    {RedesII:0},
+    
+       
 ]
 
 export {ContadormMatarias}
