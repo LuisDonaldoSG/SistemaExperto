a3a11561ec2053306ae8b918e301f94d8f908940
Jorge Ramírez Barrios <201760332@ucc.mx>
2021-11-23 14:46:26 -0600

    Correccines en materias

--- src/tools/materias.js
+++ src/tools/materias.js
@@ -1,4 +1,5 @@
 const ContadorMatarias = [
+    //Materias optativas
     {materia: 'Multimedios', contador :0},
     {materia: 'Disenoydesarrollodevideojuegos', contador:0},
     {materia: 'Comercioelectronico', contador:0},
@@ -12,21 +13,32 @@ const ContadorMatarias = [
     {materia: 'Gestiondeproyectosinformaticos', contador:0},
     {materia: 'Sistemasdeinformacionintegral', contador:0},
 
+    //Especialidad: No se porque la tienen aqui
     {materia: 'RedesIII', contador:0},
     {materia: 'RedesIV', contador:0},
     {materia: 'Serviciosdered', contador:0},
     {materia: 'Seguridadyauditoriadesistemas', contador:0},
 
+    {materia: 'Telecomunicaciones', contador:0},
+
+    //Especialidad: No se porque la tienen aqui
     {materia: 'ProgramacionwebI', contador:0},
     {materia: 'ProgramacionwebII', contador:0},
     {materia: 'Programaciondesistemasmoviles', contador:0},
     {materia: 'ProgramaciondesistemasmovilesII', contador:0},
+
+    {materia: 'Desarrollodeaplicaciones', contador:0},
     
+    //Especialidad: No se porque la tienen aqui
     {materia: 'Electronica', contador:0},
     {materia: 'Teoriadecircuitos', contador:0},
     {materia: 'Sensoresyactuadores', contador:0},
     {materia: 'SensoresyactuadoresII', contador:0},
     
+    {materia: 'Circuitosyelectronica', contador:0},
+
+    //Formación profesional
+    //No se necesitan en el contador ya que son obligatorías y no se pueden omitir
     {materia: 'Introduccionalaprogramacion', contador:0},
     {materia: 'Tecnologiasdeinformacionycomunicacion', contador:0},
     {materia: 'ProgramacionI', contador:0},
@@ -40,6 +52,8 @@ const ContadorMatarias = [
     {materia: 'BasededatosII', contador:0},
     {materia: 'RedesII', contador:0},
     
+    //Tronco común
+    //No se necesitan en el contador ya que son obligatorías a menos que se puedan omitir, ejemplo de esto inglés.
     {materia: 'Metodologiadelainvestigacion', contador:0},
     {materia: 'Administracion', contador:0},
     {materia: 'Contabilidadfinanciera', contador:0},
